76ba931677708be977d38593aa6775a1f8363242
dnvpimenta <dnvpimenta@gmail.com>
2025-01-13 16:51:01 -0300

    segundo commit

--- Desafio2dofelipao/Index2.js
+++ Desafio2dofelipao/Index2.js
@@ -1,29 +1,22 @@
-// Ranking
-
 let nickName = "Diego Pimenta"
-let result = diference(129, 35)
+let result = diference(230, 20)
 
 function diference(victories, defeats){
-
     return victories - defeats
 }
 
 let rankings = [
-
     {trophies: [0, 10], ranking: "Iron!"},
     {trophies: [11, 20], ranking: "Bronze!"},
     {trophies: [21, 50], ranking: "Silver!"},
     {trophies: [51, 80], ranking: "Gold!"},
     {trophies: [81, 90], ranking: "Diamond!"},
     {trophies: [91, 100], ranking: "Legendary!"},
-    {trophies: [101], ranking: "Imortal!"},
-        
+    {trophies: [101,1000], ranking: "Imortal!"},
 ]
 
 let ranking;
-
     for(let i = 0; i < rankings.length; i++){
-    
         if(result >= rankings[i].trophies[0] && result <= rankings[i].trophies[1]){
         ranking = rankings[i].ranking;
         break;
